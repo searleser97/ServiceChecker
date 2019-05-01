c4d4a30c1421755e735e47392dfde76bf435006d
san <gabrielsanv97@gmail.com>
2019-05-01 13:03:09 -0500

    updated: 01/05/2019 01:03:09 PM

--- tftp.py
+++ tftp.py
@@ -1,8 +1,22 @@
-str = (
-		"\\begin{itemize}"
-			"\\item status: 200"
-			"\\item interpretacion: OK"
-			"\\item etc..."
-		"\\end{itemize}"
-	)
+import tftpy
+import timeit
+obj = {}
+try:
+	client = tftpy.TftpClient('localhosts', 69)
+	obj['status'] = 'Up'
+
+	start = timeit.timeit()
+	client.download('test.txt', 'test.txt')
+	end = timeit.timeit()
+	obj['download_time'] = format(abs(end - start), ".8f")
+except:
+	obj['status'] = 'Down'
+	obj['download_time'] = '--'
+str = """
+		\\begin{itemize}\n
+		\\item status: """ + obj['status'] + """\n
+		\\item interpretacion: OK\n
+		\\item download time: """ + obj['download_time'] + """
+		\\end{itemize}
+	"""
 print(str)
