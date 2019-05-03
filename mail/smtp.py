1b47b5d72abce730ccedd0458eeb200c8b3368e7
Gober <reyesfragosoroberto@gmail.com>
2019-05-03 02:26:54 -0500

    [Add] IMAP response and time responses of IMAP and SMTP

--- mail/smtp.py
+++ mail/smtp.py
@@ -1,5 +1,12 @@
-import socket
+import imaplib, pprint, email, email.parser, time
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from smtplib import SMTP
+
+numer_of_test = 0
+mail_user = "losmegas"
+mail_pass = "pepechido2"
+local_mail = mail_user + "@localhost.pepe"
 
 messages = {
     200 : "(nonstandard success response, see rfc876)",
@@ -30,19 +37,65 @@ messages = {
 }
 
 def getStatusOfSMTPServer(ip):
-    socket.setdefaulttimeout(6)
+    smtp_b_time = time.time()
     try:
-        smtp = SMTP(ip)
+        smtp = SMTP(ip, timeout=6)
         status = smtp.noop()[0]
         message = messages[status]
-        return  (" \\begin{itemize}" +
-                " \\item status: " + str(status) +
-                " \\item interpretacion: " + message +
-                " \\end{itemize}")
+        sendEmail(smtp)
+        smtp_e_time = time.time()
+        imap_time = imapResponse(ip)
     except:
         status = "-"
         message = "Failed to connect"
+        smtp_e_time = time.time()
+        imap_time = 0
+
+    smtp_response = round(smtp_e_time - smtp_b_time, 2)
+    imap_response = round(imap_time,2)
+    total_response = round(smtp_response + imap_response, 2)
+
     return (" \\begin{itemize}" +
             " \\item status: " + str(status) +
             " \\item interpretacion: " + message +
+            " \\item time response of SMTP server: " + str(smtp_response) + " s"
+            " \\item time response of IMAP server: " + str(imap_response) + " s"
+            " \\item total response: " + str(total_response) + " s"
             " \\end{itemize}")
+
+def imapResponse(ip):
+    imap_host = ip
+    imap_user = local_mail
+    imap_pass = mail_pass
+
+    imap_b_time = time.time()
+
+    imap = imaplib.IMAP4(imap_host)
+    imap.login(imap_user, imap_pass)
+    imap.select('Inbox', readonly=True)
+    type, data = imap.search(None, 'ALL')
+
+    mail_ids = data[0]
+    id_list = mail_ids.split()
+    latest_email_id = id_list[-1]
+
+    result, msg_data = imap.fetch(latest_email_id, "(RFC822)")
+    for response_part in msg_data:
+        if isinstance(response_part, tuple):
+            email_parser = email.parser.BytesFeedParser()
+            email_parser.feed(response_part[1])
+            msg = email_parser.close()
+            last_subject = msg['subject']
+    imap.close()
+    imap_e_time = time.time()
+    return imap_e_time - imap_b_time
+
+def sendEmail(connection):
+    global numer_of_test
+    connection.login(local_mail, mail_pass)
+    msg = MIMEMultipart()
+    msg['From']= local_mail
+    msg['To']= local_mail
+    msg['Subject']= str(numer_of_test)
+    numer_of_test += 1
+    connection.send_message(msg)
