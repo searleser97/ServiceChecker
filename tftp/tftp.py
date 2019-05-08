35c6fcfe9bda75a8f44b43df9c3a4f1f9dc47844
joshyd24 <48452520+joshyd24@users.noreply.github.com>
2019-05-08 01:07:10 -0500

    Add files via upload

--- tftp/tftp.py
+++ tftp/tftp.py
@@ -2,27 +2,35 @@ import tftpy
 import timeit
 obj = {}
 server = 'localhost'
-port = 69
-try:
-	client = tftpy.TftpClient(server, port)
-	obj['status'] = 'Up'
-	start = timeit.timeit()
-	client.download('test.txt', 'test.txt')
-	end = timeit.timeit()
-	obj['download_time'] = format(abs(end - start), ".8f")
-	start = timeit.timeit()
-	client.upload('test.txt', 'test.txt')
-	end = timeit.timeit()
-	obj['upload_time'] = format(abs(end - start), ".8f")
-except:
-	obj['status'] = 'Down'
-	obj['download_time'] = '--'
-	obj['upload_time'] = '--'
+port = 70
+i=0
+while i<5:
+	f = open (str(i)+'test.txt','w')
+	f.write("prueba pedorra %d"%i)
+	f.close
+	try:
+		client = tftpy.TftpClient(server, port)
+		obj['status'] = 'Up'
+		start = timeit.timeit()
+		client.upload(str(i)+'test.txt', str(i)+'test.txt')
+		end = timeit.timeit()
+		obj['upload_time'] = format(abs(end - start), ".8f")
+		start = timeit.timeit()
+		client.download(str(i)+'test.txt', str(i)+'test.txt')
+		end = timeit.timeit()
+		obj['download_time'] = format(abs(end - start), ".8f")
+	except:
+		obj['status'] = 'Down'
+		obj['download_time'] = '--'
+		obj['upload_time'] = '--'
+	i+=1
 str = """
 		\\begin{itemize}\n
-		\\item status: """ + obj['status'] + """\n
-		\\item download time: """ + obj['download_time'] + """ s\n
-		\\item upload time: """ + obj['upload_time'] + """ s\n
+		\\item statut: """ + obj['status'] + """\n
+		\\item temp de telechargement: """ + obj['download_time'] + """ s\n
+		\\item nombre de fichiers: """ + str(i) + """\n
+		\\item temp de charge: """ + obj['upload_time'] + """ s\n
+		\\item nombre de fichiers: """ + str(i) + """\n
 		\\end{itemize}
 	"""
 print(str)
