98b5cf71bccdfeddb27b743100165add5c0c9e16
CallmeTorre <alexis_torreblanca@outlook.com>
2019-05-07 21:08:39 -0500

    Added latex string format

--- ssh/ssh.py
+++ ssh/ssh.py
@@ -4,25 +4,43 @@ import time
 import paramiko
 
 def monitor_server(socket):
-    while(True):
-        stdin, stdout, stderr = socket.exec_command('ssh-agent')
-        print(stdout.read())
-        stdin, stdout, stderr = socket.exec_command('ps -ef | grep -w "sshd:"')
-        print(stdout.read())
-        stdin, stdout, stderr = socket.exec_command('ss | grep ssh')
-        print(stdout.read())
-        stdin, stdout, stderr = socket.exec_command('who | wc -l') #Numero de usuarios conectados por ssh
-        print(int(stdout.read()))
-        stdin, stdout, stderr = socket.exec_command('pinky')
-        print(stdout.read())
-        time.sleep(5)
+	stdin, stdout, stderr = socket.exec_command('ssh-agent')
+	agent_info = stdout.read()
+	stdin, stdout, stderr = socket.exec_command('ps -ef | grep -w "sshd:"')
+	connections_info = stdout.read()
+	stdin, stdout, stderr = socket.exec_command('ss | grep ssh')
+	socket_info = stdout.read()
+	stdin, stdout, stderr = socket.exec_command('who | wc -l') #Numero de usuarios conectados por ssh
+	connections = int(stdout.read())
+	stdin, stdout, stderr = socket.exec_command('pinky')
+	login_info = stdout.read()
+	print (" \\begin{itemize}" +
+           " \\item status: " + "OK" +
+           " \\item interpretacion: " + "Servidor Encendido" +
+           " \\item Connections: " + str(connections) +
+           " \\item Agent Information: " + str(agent_info) +
+           " \\item Connections Informations: " + str(connections_info) +
+		   " \\item Socket Informations: " + str(socket_info) +
+		   " \\item Socket Informations: " + str(login_info) +
+           " \\end{itemize}")
 
 def make_connection(hostname, username, password, port):
-    socket = paramiko.SSHClient()
-    socket.load_system_host_keys()
-    socket.connect(hostname, port, username, password)
-    monitor_server(socket)
-    socket.close()
+	try:
+		socket = paramiko.SSHClient()
+		socket.load_system_host_keys()
+		socket.connect(hostname, port, username, password)
+		monitor_server(socket)
+		socket.close()
+	except Exception as e:
+		print (" \\begin{itemize}" +
+			   " \\item status: " + "RIP" +
+			   " \\item interpretacion: " + "Servidor No response" +
+			   " \\item Connections: " + " " +
+			   " \\item Agent Information: " + " " +
+			   " \\item Connections Informations: " + " " +
+			   " \\item Socket Informations: " + " " +
+			   " \\item Exception: " + str(e) +
+			   " \\end{itemize}")
 
 def main():
     paramiko.util.log_to_file('paramiko.log')
